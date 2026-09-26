8081c34eff09fde82aabfda2cc5ff5916dd6b4ec
EUNSOLY <dmsthf9596@gmail.com>
2026-09-26 10:10:22 +0900

    🐛bug: 연동작업 targetDate 컬럼 오표기 수정

--- src/main/java/com/eunsoly/findex/application/integration/dto/SyncIndexResult.java
+++ src/main/java/com/eunsoly/findex/application/integration/dto/SyncIndexResult.java
@@ -2,6 +2,7 @@ package com.eunsoly.findex.application.integration.dto;
 
 import com.eunsoly.findex.domain.entity.index.IndexInformation;
 import com.eunsoly.findex.domain.entity.integration.IntegrationHistory;
+
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
@@ -10,8 +11,8 @@ public record SyncIndexResult(Long id, String jobType, Long indexInfoId, String 
     public static SyncIndexResult of(IntegrationHistory integrationHistory, IndexInformation indexInformation) {
         Long indexInformationId = Optional.ofNullable(indexInformation).map(IndexInformation::getId).orElse(null);
 
-        String targetDate =
-                Optional.ofNullable(integrationHistory.getJobTime()).map(date -> date.format(DateTimeFormatter.ofPattern("yyyyMMdd"))).orElse(null);
+        String targetDate = Optional.ofNullable(integrationHistory.getTargetDate()).map(date -> date.format(DateTimeFormatter.ofPattern("yyyyMMdd")))
+                .orElse(null);
 
         String jobTime = Optional.ofNullable(integrationHistory.getJobTime())
                 .map(time -> time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))).orElse(null);
